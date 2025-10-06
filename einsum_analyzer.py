d3f78e3fbc44d00d131ccb94d6cb1bc182c9dc02
willmo3 <thelordofbouge@gmail.com>
2025-10-06 10:25:13 -0400

    Add support for counting reads to analyzer

--- einsum_analyzer.py
+++ einsum_analyzer.py
@@ -29,24 +29,33 @@ class EinsumVisitor(ABC):
         pass
 
 class CountAssignVisitor(EinsumVisitor):
-    def __init__(self):
+    """
+    :param env Mapping of dimension titles to their size.
+    Tensors with dimensions of matching titles are assumed to match in size.
+    """
+    def __init__(self, env):
+        self._env = env
         self._dims = set()
         self._tensor_index_map = dict()
 
+    def reset(self):
+        self.__init__(self._env)
+
+    # Visitor methods
+
     def apply_literal(self, node):
         pass
-
     def apply_access(self, node):
         for dim in node.idxs:
             self._dims.add(dim)
 
         if node.tns not in self._tensor_index_map:
             self._tensor_index_map[node.tns] = set()
-        self._tensor_index_map[node.tns].add(tuple(node.idxs))
 
+        for idx in node.idxs:
+            self._tensor_index_map[node.tns].add(idx)
     def apply_einsum(self, node):
         pass
-
     def apply_call(self, node):
         pass
 
@@ -56,6 +65,23 @@ class CountAssignVisitor(EinsumVisitor):
         for tns in self._tensor_index_map:
             print(tns, self._tensor_index_map[tns])
 
-    def generate_iter_order(self):
+    def report_example_iter_order(self):
         for dim in self._dims:
             print(dim)
+
+    """
+    Report the total reads a matrix operation will require in a given env.
+    
+    This is more complex than simply multiplying the size of the dimensions, because some dimensions are iterated over multiple times.
+    """
+    def report_total_reads(self):
+        factors = { dim: 0 for dim in self._dims }
+        for tns in self._tensor_index_map:
+            for idx in self._tensor_index_map[tns]:
+                factors[idx] += 1
+
+        cost = 1
+        for dim in self._dims:
+            cost = cost * factors[dim] * self._env[dim]
+
+        return cost
--- test.py
+++ test.py
@@ -1,14 +1,49 @@
 from einsum_analyzer import CountAssignVisitor
 from einsum_parser import parse_einsum
 
-# Matrix addition with transpose
-C = parse_einsum("C[i,j] = A[i,j] + B[j,i]")
-# Matrix multiplication
-D = parse_einsum("D[i,j] += A[i,k] * B[k,j]")
-# Min-Plus multiplication with shift
-E = parse_einsum("E[i] min= A[i,k] + D[k,j] << 1")
+"""
+Examples considered:
 
-visitor = CountAssignVisitor()
-visitor.visit(E)
-visitor.generate_iter_order()
-visitor.report_traversals()
+Matrix addition with transpose:
+C[i,j] = A[i,j] + B[j,i]")
+
+Matrix multiplication
+D[i,j] += A[i,k] * B[k,j]")
+
+Min-Plus multiplication with shift
+E[i] min= A[i,k] + D[k,j] << 1
+"""
+
+# -- Shared State -- #
+
+# Dimensions mapped to their size.
+env = {
+    "i": 2,
+    "k": 3,
+    "j": 4,
+}
+visitor = CountAssignVisitor(env)
+
+# -- Test functions -- #
+
+def test_report_reads():
+    tree = parse_einsum("E[i] min= A[i,k] + D[k,j] << 1")
+    visitor.visit(tree)
+    cost = visitor.report_total_reads()
+    assert cost == 48
+
+    visitor.reset()
+    tree = parse_einsum("C[i,j] = A[i,j] + B[j,i]")
+    visitor.visit(tree)
+    cost = visitor.report_total_reads()
+    assert cost == 32
+
+    visitor.reset()
+    tree = parse_einsum("D[i,j] += A[i,k] * B[k,j]")
+    visitor.visit(tree)
+    cost = visitor.report_total_reads()
+    assert cost == 48
+
+# -- Execution -- #
+
+test_report_reads()
